54ba78f90a3a8dc6b4a5d834d4ff3828b66ad783
robrib1820 <rribeiro1@byupathway.edu>
2026-02-19 17:06:51 -0300

    W06 Code: Trees completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,7 +12,11 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
+        if (value == Data)
+        {
+            return;
+        }
+        
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +38,35 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;
+            }
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left == null ? 0 : Left.GetHeight();
+        int rightHeight = Right == null ? 0 : Right.GetHeight();
+
+        return Math.Max(leftHeight, rightHeight) + 1; // Replace this line with the correct return statement(s)
     }
 }
